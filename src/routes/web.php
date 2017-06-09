165c1b58426b1814e76f3a5ed97cafaafd9d6afd
Marko Banusic <mbanusic@gmail.com>
2017-06-09 22:14:17 +0200

    Rename some routes

--- src/routes/web.php
+++ src/routes/web.php
@@ -3,16 +3,16 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::group(['namespace' => 'Inspirium\HumanResources\Controllers', 'middleware' => ['web', 'auth'], 'prefix' => 'hr'], function() {
+Route::group(['namespace' => 'Inspirium\HumanResources\Controllers', 'middleware' => ['web', 'auth'], 'prefix' => 'human_resources'], function() {
+    Route::get('employees', 'EmployeeController@showEmployees');
     Route::group(['prefix' => 'employee'], function () {
-        Route::get('/', 'EmployeeController@showEmployees');
-        Route::get('show/{id}', 'EmployeeController@showEmployee');
         Route::get('edit/{id?}', 'EmployeeController@showEditEmployee');
         Route::post('edit/{id?}', 'EmployeeController@submitEmployee');
         Route::get('delete/{id}', 'EmployeeController@deleteEmployee');
+        Route::get('show/{id}', 'EmployeeController@showEmployee');//TODO: remove show
     });
-    Route::group(['prefix' => 'department'], function() {
-        Route::get('/', 'DepartmentController@showDepartments');
+    Route::get('departments', 'DepartmentController@showDepartments');
+    Route::group(['prefix' => 'departments'], function() {
         Route::get('edit/{id?}', 'DepartmentController@showDepartment');
         Route::post('edit/{id?}', 'DepartmentController@submitDepartment');
         Route::get('delete/{id}', 'DepartmentController@deleteDepartment');
