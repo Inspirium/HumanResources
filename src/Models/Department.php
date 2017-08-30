a4cf9233205fb2f7b208cb3fb346b200d714a1e4
Marko Banusic <mbanusic@gmail.com>
2017-08-30 06:37:16 +0200

    Department tasks

--- src/Models/Department.php
+++ src/Models/Department.php
@@ -15,5 +15,9 @@ class Department extends Model {
     public function employees() {
         return $this->hasMany('Inspirium\HumanResources\Models\Employee');
     }
+
+	public function tasks() {
+		return $this->belongsToMany('Inspirium\TaskManagement\Models\Task', 'department_task_pivot', 'department_id', 'task_id')->with('assigner')->withPivot('order')->orderBy('pivot_order');
+	}
 }
 
