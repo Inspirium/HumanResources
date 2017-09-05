c61ceed91920eb2aa19bd234d601706062681cee
Marko Banusic <mbanusic@gmail.com>
2017-09-05 14:27:41 +0200

    Remove meta

--- src/Models/Department.php
+++ src/Models/Department.php
@@ -2,13 +2,8 @@
 namespace Inspirium\HumanResources\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Phoenix\EloquentMeta\MetaTrait;
 
 class Department extends Model {
-
-    use MetaTrait;
-
-    protected $meta_model = 'Inspirium\HumanResources\Models\DepartmentsModelMeta';
 
     protected $fillable = ['name'];
 
--- src/Models/Employee.php
+++ src/Models/Employee.php
@@ -3,13 +3,8 @@
 namespace Inspirium\HumanResources\Models;
 
 use Illuminate\Database\Eloquent\Model;
-use Phoenix\EloquentMeta\MetaTrait;
 
 class Employee extends Model {
-
-    use MetaTrait;
-
-    protected $meta_model = 'Inspirium\HumanResources\Models\EmployeeModelMeta';
 
     protected $fillable = ['first_name', 'last_name', 'email', 'department_id'];
     protected $appends = ['name', 'phone', 'mobile', 'room', 'department_name'];
