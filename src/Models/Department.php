c64158cfb7c44f51cb6fc4695d7d87bb661dd51f
Marko Banusic <mbanusic@gmail.com>
2017-10-14 01:23:18 +0200

    Docs for models

--- src/Models/Department.php
+++ src/Models/Department.php
@@ -3,6 +3,21 @@ namespace Inspirium\HumanResources\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Inspirium\HumanResources\Models\Department
+ *
+ * @property int $id
+ * @property string $name
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Inspirium\HumanResources\Models\Employee[] $employees
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Inspirium\TaskManagement\Models\Task[] $tasks
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Department whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Department whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Department whereName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Department whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Department extends Model {
 
     protected $fillable = ['name'];
--- src/Models/Employee.php
+++ src/Models/Employee.php
@@ -4,6 +4,58 @@ namespace Inspirium\HumanResources\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Inspirium\HumanResources\Models\Employee
+ *
+ * @property int $id
+ * @property string $first_name
+ * @property string $last_name
+ * @property string|null $email
+ * @property int|null $user_id
+ * @property int|null $department_id
+ * @property string|null $image
+ * @property string|null $mobile_pre
+ * @property string|null $mobile
+ * @property string|null $mobile_vpn
+ * @property string|null $phone_pre
+ * @property string|null $phone
+ * @property string|null $phone_vpn
+ * @property string|null $address
+ * @property string|null $city
+ * @property string|null $postal_code
+ * @property string|null $room
+ * @property string|null $sex
+ * @property \Carbon\Carbon|null $created_at
+ * @property \Carbon\Carbon|null $updated_at
+ * @property string|null $deleted_at
+ * @property-read \Inspirium\HumanResources\Models\Department|null $department
+ * @property-read mixed $department_name
+ * @property-read mixed $name
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Inspirium\TaskManagement\Models\Task[] $tasks
+ * @property-read \Inspirium\UserManagement\Models\User|null $user
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereCity($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereDeletedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereDepartmentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereImage($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereMobile($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereMobilePre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereMobileVpn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee wherePhone($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee wherePhonePre($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee wherePhoneVpn($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee wherePostalCode($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereRoom($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereSex($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|\Inspirium\HumanResources\Models\Employee whereUserId($value)
+ * @mixin \Eloquent
+ */
 class Employee extends Model {
 
     protected $fillable = ['first_name', 'last_name', 'email', 'department_id'];
